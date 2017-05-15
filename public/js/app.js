4c8099a6a0559723b508817a548458e6e8dff8c6
Antuan Caraballo <tranc2606@gmail.com>
2017-05-14 17:07:41 -0700

    added some basic front end form to submit a message

--- public/js/app.js
+++ public/js/app.js
@@ -11,3 +11,22 @@ socket.on('message', function (message){
 	console.log('New message here!');
 	console.log(message.text);
 });
+
+
+//--> This handles the submitting of new message
+var $form = jQuery('#message-form');
+$form.on('submit', function (event) {
+
+//--> OJO: this function lets a form to not be submitted the old way, which refreshes the entire page
+//         when using socket io, you want to use this function, so that the form handles the
+//         submission on its own	
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+	socket.emit('message', {   
+		text: $message.val() //-> this selects all input tags whose names are equal to message
+	});                                    //--> .val() pulls the value out
+
+	$message.val(''); //-> this resets the input
+ });
+
